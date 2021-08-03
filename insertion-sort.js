3ea73ed8f329c5de7e5788e77272b084be82471c
nummyrice <nicholasroyrice@gmail.com>
2021-08-03 10:22:15 -0700

    almost done

--- insertion-sort.js
+++ insertion-sort.js
@@ -4,26 +4,46 @@
 function insertionSort(arr) {
 
   // Copy the original array
+  let arrcpy = arr.slice(0);
 
   // Create an array to store the sorted values
+  let sorted = [];
 
   // While the array is not empty...
+  while (arrcpy.length) {
 
     // Do not move this console.log
     console.log(sorted.join(","));
 
     // Pop a value from the array
+    let val = arrcpy.pop();
 
     // Create a new spot at the end of the array
-
+    sorted.push(null);
+    debugger;
+    let insertionPt = 0;
     // Walk through the sorted array in reverse order
       // Check if the value to the left is smaller than the new value
         // If so, you've reached the insertion point so exit the loop
         // If not shift the value to the right by 1 and continue
-
+    for (let i = sorted.length - 2; i >= 0; i--) {
+      if (!sorted[i] < val) {
+        sorted[i + 1] = sorted[i];
+      } else {
+        insertionPt = i;
+        break;
+      }
+      debugger;
+    }
+    sorted[insertionPt] = val;
+    debugger;
+  }
     // Insert the unsorted value at the break point
 
+
   // Return the sorted array
+  console.log(sorted);
+  return sorted;
 
 }
 
@@ -52,4 +72,3 @@ function insertionSortInPlace(arr) {
 }
 
 module.exports = [insertionSort, insertionSortInPlace];
-
